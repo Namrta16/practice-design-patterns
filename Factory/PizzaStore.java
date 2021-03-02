bc857b07206402357fedbe1e7592764bbef76c96
Namrta <knamrtasai@gmail.com>
2021-03-02 11:22:25 -0800

    factory pattern

--- Factory/PizzaStore.java
+++ Factory/PizzaStore.java
@@ -1,1 +1,12 @@
 // A factory pattern is a creational pattern that abstracts or hides the object creation process.
+//// pizza store is handling instantiation of the appropriate pizza type
+
+public class PizzaStore {
+  public Pizza orderPizza(String type) {
+    Pizza p = PizzaFactory.createPizza(type);
+    p.prepare();
+    p.bake();
+    p.cut();
+  }
+
+}
---
+++ Factory/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+  public static void main (String[] args) {
+    PizzaStore ps = new PizzaStore();
+    ps.orderPizza("veggie");
+  }
+}
